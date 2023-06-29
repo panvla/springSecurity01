62234d02f131e91757a700ec9f11331ef3110230
panvla <pandvlado@gmail.com>
2023-06-29 14:57:12 +0200

    initial spring project added with some basic application.yml configuration

---
+++ springSecurity01B/src/main/java/com/vladimirpandurov/springSecurity01B/SpringSecurity01BApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.springSecurity01B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity01BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity01BApplication.class, args);
+	}
+
+}
---
+++ springSecurity01B/src/test/java/com/vladimirpandurov/springSecurity01B/SpringSecurity01BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.springSecurity01B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity01BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
