dde6d353087a0a4e77bc8772d3d2296ece2b3e57
panvla <pandvlado@gmail.com>
2023-06-30 10:45:57 +0200

    User model defined.closes #2.

---
+++ springSecurity01B/src/main/java/com/vladimirpandurov/springSecurity01B/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.vladimirpandurov.springSecurity01B.repository;
+
+import com.vladimirpandurov.springSecurity01B.domain.User;
+
+import java.util.Collection;
+
+public interface UserRepository<T extends User> {
+    /* Basic CRUD Operations */
+    T create(T data);
+    Collection<T> list(int page, int pageSize);
+    T get(Long id);
+    T update(T data);
+    Boolean delete(Long id);
+
+    /* More Complex Operations */
+}
